e892fdc72f18771da694c06bc850378bee8e9295
lostimage <aisovik87@gmail.com>
2018-06-03 23:17:54 +0300

    media added

--- js/main.js
+++ js/main.js
@@ -4,7 +4,18 @@
 		var slider = $('.ba-full-slider').slick({
 			  arrows:false,
 			  dots:true,
-			  slide:'.ba-full-slide'
+			  slide:'.ba-full-slide',
+			 
+
+			  responsive: [
+				{
+				  breakpoint: 480,
+				  settings: {
+					dots:false,
+				  }
+				}
+			  ]
+
 
 		  });
 		 	
@@ -14,20 +25,42 @@
 			slide: '.ba-news__slide',
 			slidesToShow:3,
 			slidesToScroll:1,
+			adaptiveHeight: true,
 			prevArrow:('.icon-cheveron-outline-left'),
 			nextArrow:('.icon-cheveron-outline-right'),
- 			autoplay: true,
+ 			// autoplay: true,
 			autoplaySpeed: 5000,
- 			appendDots:('.ba-news')
+			 appendDots:('.ba-news'),
+			 responsive: [
+				{
+				  breakpoint: 1200,
+				  settings: {
+					slidesToShow:2,
+					slidesToScroll:1,
+				  }
+				},
+				{
+					breakpoint: 800,
+					settings: {
+					  slidesToShow:1,
+					  slidesToScroll:1,
+					}
+				  }
+			  ]
 		});
-		$(".ba-header__nav, .ba-scroll").on("click","a", function (event) {
+		$(".ba-header__nav, .ba-scroll, #mobile-menu").on("click","a", function (event) {
 					event.preventDefault();
 					var id  = $(this).attr('href'),
 						top = $(id).offset().top;
 					
 					$('body,html').animate({scrollTop: top}, 1500);
 				});
-			
+				var nav = $('#mobile-menu') ,
+				menuToggle = $('.ba-menu-button, #mobile-menu') ;
+				menuToggle.on('click' , function (e) {
+					e.preventDefault();
+					nav.toggleClass('ba-open');
+				});
 		
 })(jQuery);
 
